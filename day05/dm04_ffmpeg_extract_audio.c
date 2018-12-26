e3983e02a8d901f1c57dc620919c41075d3072fc
王永杰 <wangyongjie@kugou.net>
2018-12-26 14:29:05 +0800

    day05#feat:测试抽取音频流

--- day05/dm04_ffmpeg_extract_audio.c
+++ day05/dm04_ffmpeg_extract_audio.c
@@ -1,6 +1,40 @@
+#include <stdio.h>
 #include <libavutil/log.h>
 #include <libavformat/avformat.h>
-#include <stdio.h>
+
+#define ADTS_HEADER_LEN 7;
+
+void adts_header(char *szAdtsHeader, int dataLen){
+
+    int audio_object_type = 2;
+    int sampling_frequency_index = 7;
+    int channel_config = 2;
+
+    int adtsLen = dataLen + 7;
+
+    szAdtsHeader[0] = 0xff;         //syncword:0xfff                          高8bits
+    szAdtsHeader[1] = 0xf0;         //syncword:0xfff                          低4bits
+    szAdtsHeader[1] |= (0 << 3);    //MPEG Version:0 for MPEG-4,1 for MPEG-2  1bit
+    szAdtsHeader[1] |= (0 << 1);    //Layer:0                                 2bits
+    szAdtsHeader[1] |= 1;           //protection absent:1                     1bit
+
+    szAdtsHeader[2] = (audio_object_type - 1)<<6;            //profile:audio_object_type - 1                      2bits
+    szAdtsHeader[2] |= (sampling_frequency_index & 0x0f)<<2; //sampling frequency index:sampling_frequency_index  4bits
+    szAdtsHeader[2] |= (0 << 1);                             //private bit:0                                      1bit
+    szAdtsHeader[2] |= (channel_config & 0x04)>>2;           //channel configuration:channel_config               高1bit
+
+    szAdtsHeader[3] = (channel_config & 0x03)<<6;     //channel configuration:channel_config      低2bits
+    szAdtsHeader[3] |= (0 << 5);                      //original：0                               1bit
+    szAdtsHeader[3] |= (0 << 4);                      //home：0                                   1bit
+    szAdtsHeader[3] |= (0 << 3);                      //copyright id bit：0                       1bit
+    szAdtsHeader[3] |= (0 << 2);                      //copyright id start：0                     1bit
+    szAdtsHeader[3] |= ((adtsLen & 0x1800) >> 11);           //frame length：value   高2bits
+
+    szAdtsHeader[4] = (uint8_t)((adtsLen & 0x7f8) >> 3);     //frame length:value    中间8bits
+    szAdtsHeader[5] = (uint8_t)((adtsLen & 0x7) << 5);       //frame length:value    低3bits
+    szAdtsHeader[5] |= 0x1f;                                 //buffer fullness:0x7ff 高5bits
+    szAdtsHeader[6] = 0xfc;
+}
 
 int main(int argc, char *argv[]) {
   int ret = 0;
@@ -17,7 +51,7 @@ int main(int argc, char *argv[]) {
     av_log(NULL, AV_LOG_ERROR, "the count of params should be more than three!\n");
     return -1;
   }
-
+  //1 read tow params from console
   src = argv[1];
   dst = argv[2];
 
@@ -52,9 +86,14 @@ int main(int argc, char *argv[]) {
 
   audio_index = ret;
   av_init_packet(&pkt);
-  while(av_read_frame(fmt_ctx, pkt) >= 0) {
+  while(av_read_frame(fmt_ctx, &pkt) >= 0) {
     if (pkt.stream_index == audio_index) {
-      len = fwrite(pkt.data, 1, pkt.size, dst_fd);
+      char adts_header_buf[7];
+      adts_header(adts_header_buf, pkt.size);
+      fwrite(adts_header_buf, 1, 7, dst_fd);
+      
+      //3 write audio data to aac file
+      int len = fwrite(pkt.data, 1, pkt.size, dst_fd);
       if (len != pkt.size) {
         av_log(NULL, AV_LOG_WARNING, "warning, length of data is not equal the size of pkt!\n");
       }
--- day05/run.sh
+++ day05/run.sh
@@ -1,8 +1,17 @@
+#测试log功能
 clang -g -o dm01_ffmpeg_log dm01_ffmpeg_log.c  -lavutil
 
+#测试删除移动文件
 clang -g -o dm03_ffmpeg_file dm03_ffmpeg_file.c  -lavutil -lavformat
 clang -g -o dm03_ffmpeg_file dm03_ffmpeg_file.c `pkg-config --libs libavformat libavutil`
 
+#测试读目录
 clang -g -o dm04_ffmpeg_dir dm04_ffmpeg_dir.c `pkg-config --libs libavformat libavutil`
 
+#dump流信息
 clang -g -o dm04_ffmpeg_mediainfo dm04_ffmpeg_mediainfo.c -I /usr/local/Cellar/ffmpeg/4.1_1/include -L /usr/local/Cellar/ffmpeg/4.1_1/lib -lavutil -lavformat
+clang -g -o dm04_ffmpeg_mediainfo dm04_ffmpeg_mediainfo.c `pkg-config --libs libavformat libavutil`
+
+#抽取音频流
+clang -g -o dm04_ffmpeg_extract_audio dm04_ffmpeg_extract_audio.c `pkg-config --libs libavformat libavutil`
+./dm04_ffmpeg_extract_audio eason.mp4 eason.aac
